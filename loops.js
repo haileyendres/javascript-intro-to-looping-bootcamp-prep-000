153b438fad92a49c8bc5796f42ce9d56601f8401
hailey endres <hailey.endres@gmail.com>
2017-08-01 18:02:17 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,37 @@
+function forLoop(array)
+{
+  for(let i= 0; i < 25; i++)
+  {
+    if(i===1)
+    {
+      array.push("I am 1 strange loop")
+    }
+    else
+    {
+      array.push(`I am &{i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(number)
+{
+  while(number>0)
+  {
+    console.log(--number)
+  }
+  return "done"
+}
+
+function doWhileLoop(array)
+{
+  do
+  {
+    function maybeTrue()
+    {
+      array.pop(Math.random() >= 0.5) // Returns a random number between 0 (inclusive) and 1 (exclusive)
+    }
+  }
+  while(array.length > 0 && maybeTrue())
+  return array
+}
